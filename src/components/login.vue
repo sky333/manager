d8e8089ed68707e79a3660c4b032e9e9720beb78
sky333 <22768972@qq.com>
2019-03-10 15:19:39 +0800

    完成了登录表单样式

--- src/components/login.vue
+++ src/components/login.vue
@@ -1,13 +1,105 @@
 <template>
-    <div>
-        登录页
-    </div>
+  <div id="login">
+    <!-- 要给标签绑定数据 :formData   绑定验证规则 :rulss   -->
+    <el-form
+      :model="formData"
+      status-icon
+      ref="formData"
+      label-width="100px"
+      class="demo-ruleForm"
+      label-position="top"
+      :rules="rules"
+    >
+      <h2>用户登录</h2>
+      <el-form-item label="用户名" prop="username">
+        <el-input v-model.trim="formData.username"></el-input>
+      </el-form-item>
+      <el-form-item label="密码" prop="password">
+        <el-input type="password" v-model.trim="formData.password" autocomplete="off"></el-input>
+      </el-form-item>
+      <el-form-item>
+        <el-button class="btn" type="primary" @click="submitForm('formData')">提交</el-button>
+      </el-form-item>
+    </el-form>
+  </div>
 </template>
 <script>
 export default {
+  data() {
+    return {
+      //表单数据
+      formData: {
+        username: "",
+        password: ""
+      },
+      //表单的验证规则
+      rules: {
+        username: [
+          // required是否必填(会提示*),message提示文本,提示触发条件  blur:失去焦点  change:输入状态
+          { required: true, message: "请输入用户名", trigger: "blur" },
+          {
+            min: 3,
+            max: 12,
+            message: "长度在 3 到 12 个字符",
+            trigger: "change"
+          }
+        ],
+        password: [
+          { required: true, message: "请输入密码", trigger: "blur" },
+          {
+            min: 6,
+            max: 18,
+            message: "长度在 3 到 18 个字符",
+            trigger: "change"
+          }
+        ]
+      }
+    };
+  },
+  methods: {
+    submitForm(formName) {
+      this.$refs[formName].validate(valid => {
+        if (valid) {
+          alert("submit!");
+        } else {
+          console.log("error submit!!");
+          this.$message.error('请输入正确的用户名和账号');
+          return false;
+        }
+      });
+    },
     
-}
+  }
+};
 </script>
 <style>
-
+html,
+body {
+  width: 100%;
+  height: 100%;
+  margin: 0;
+  padding: 0;
+}
+body > div:first-of-type {
+  width: 100%;
+  height: 100%;
+  background-color: #324152;
+  /* 设置模块居中 */
+  display: flex;
+  /* 主轴居中 */
+  justify-content: center;
+  /* 副轴居中 */
+  align-items: center;
+}
+.el-form {
+  width: 580px;
+  height: 440px;
+  background-color: #fff;
+  border-radius: 10px;
+  box-sizing: border-box;
+  padding: 40px;
+}
+.btn {
+  width: 100%;
+}
 </style>
--- src/main.js
+++ src/main.js
@@ -3,6 +3,11 @@ import App from './App.vue'
 
 //导入路由
 import router from './lib/router'
+//导入饿了么UI
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+Vue.use(ElementUI);
+
 
 Vue.config.productionTip = false
 
